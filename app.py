bcf4659b33649a4a443dd6e140a3a942f4357354
XomaDev <bgpsdr@gmail.com>
2021-05-12 22:09:00 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from json import JSONEncoder
+from flask import Flask, request
+
+import chocolateo
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return app.response_class(
+        response='Asteroid is working',
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/api/answer')
+def answer():
+    result = chocolateo.web_scrape(request.args.get("text"))
+
+    return app.response_class(
+        response=JSONEncoder().encode({
+            "answer": result[0],
+            "source": result[1]
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/api/info')
+def info():
+    return app.response_class(
+        response=JSONEncoder().encode({
+            "answer": chocolateo.bingScrape(request.args.get('text')),
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+if __name__ == '__main__':
+    app.run()
